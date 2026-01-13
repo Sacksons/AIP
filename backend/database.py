1554006920e0b3bdc326b597b6eda10897c2e965
sackson <vamosakor@gmail.com>
2026-01-13 20:16:57 +0100

    Structure Postgres connection for verified deployment

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+DATABASE_URL = "postgresql://super:your_password@sackson-5021.postgres.pythonanywhere-services.com:15021/sackson$aipi_db"
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:secret@localhost:5432/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
