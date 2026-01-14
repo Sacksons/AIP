8bb4a612b8e8cddd1c2de2a63a822223f374311c
Sacksons <vamosakor@gmail.com>
2026-01-14 01:43:19 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,18 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:secret@localhost:5432/postgres"
-DATABASE_URL = "postgresql://super:your_password@sackson-5021.postgres.pythonanywhere-services.com:15021/sackson$aipi_db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 # backend/database.py
 from __future__ import annotations
 
